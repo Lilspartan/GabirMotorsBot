1c21c10253d9945f0f0acd1484fa80bd01c9a8b0
Lilspartan <44793856+Lilspartan@users.noreply.github.com>
2021-07-31 16:12:15 -0700

    Create quotes.js

---
+++ slashCommands/quotes.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require('discord.js');
+const color = require('../JSON/colors.json');
+const axios = require('axios');
+
+module.exports = {
+  config: {
+      name: "quote",
+      description: "Get a random quote"
+  },
+  run: async (bot, interaction, Reply) => {
+    axios.get(`https://api.gabirmotors.com/quotes`).then(quotes => {
+      const id = Math.floor(Math.random() * quotes.data.length - 1)
+      const randomHaiku = quotes.data[id]
+      const hEmbed = new MessageEmbed();
+      hEmbed.setDescription(`\`Quote Number: ${id}\`\n\n${randomHaiku.quote}`)
+      hEmbed.setColor(color.red_light);
+      hEmbed.setTimestamp();
+      hEmbed.setFooter(`Gabir Motors`);
+
+      return Reply.sendEmbed(hEmbed)
+    })
+  } 
+}
+
