bb3b995af709975e658ee92ce086e812a6e5c8ec
Lilspartan <44793856+Lilspartan@users.noreply.github.com>
2021-07-31 16:12:29 -0700

    Create team.js

---
+++ slashCommands/team.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require('discord.js');
+const color = require('../JSON/colors.json');
+const axios = require('axios');
+
+module.exports = {
+  config: {
+      name: "team",
+      description: "Get info about a team"
+  },
+  run: async (bot, interaction, Reply) => {
+    chosenOption = interaction.data.options[0].value
+    axios.get(`https://api.gabirmotors.com/team/${chosenOption}`)
+    .then(res => {
+      let embed = new MessageEmbed();
+      embed.setTitle(`**${res.data.name} | ${res.data.drivers.length} Drivers**`);
+      embed.setDescription(`Team Leader: \`${res.data.team_leader}\`\n\n[View web version](https://gabirmotors.com/teams/${res.data.abbr})\n**Drivers: **\n`)
+      res.data.drivers.forEach(d => {
+        embed.addField(`**${d.name} ${(d.username != undefined ? "(" + d.username + ")" : "")}**`, `Car No. - \`${(d.car_number != -1) ? d.car_number : "N/A"}\``, true)
+      })
+      if (res.data.logo != undefined) embed.setImage(res.data.logo)
+      embed.setColor(color.gold);
+      embed.setTimestamp();
+      embed.setFooter(`Gabir Motors`);
+      return Reply.sendEmbed(embed)
+    })
+  } 
+}
+
