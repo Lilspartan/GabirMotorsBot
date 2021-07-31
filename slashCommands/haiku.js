4b25f1c5b866a83cffefc549721a43c80bc0fa39
Lilspartan <44793856+Lilspartan@users.noreply.github.com>
2021-07-31 16:11:47 -0700

    Create haiku.js

---
+++ slashCommands/haiku.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require('discord.js');
+const color = require('../JSON/colors.json');
+const axios = require('axios');
+
+module.exports = {
+  config: {
+      name: "haiku",
+      description: "Get a random haiku"
+  },
+  run: async (bot, interaction, Reply) => {
+    axios.get(`https://api.gabirmotors.com/haikus`).then(haikus => {
+      const id = Math.floor(Math.random() * haikus.data.length - 1)
+      const randomHaiku = haikus.data[id]
+      const hEmbed = new MessageEmbed();
+      hEmbed.setDescription(`\`Haiku Number: ${id}\`\n\n${randomHaiku.haikuLines[0]}\n${randomHaiku.haikuLines[1]}\n${randomHaiku.haikuLines[2]}\n\n-${randomHaiku.name}`)
+      hEmbed.setColor(color.orange);
+      hEmbed.setTimestamp();
+      hEmbed.setFooter(`Gabir Motors`);
+
+      return Reply.sendEmbed(hEmbed)
+    })
+  } 
+}
+
