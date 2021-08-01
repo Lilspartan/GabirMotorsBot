778d150fd00e0f64a5cc3e3a7f4656f9c01c95fd
Lilspartan <44793856+Lilspartan@users.noreply.github.com>
2021-07-31 21:33:33 -0700

    Update event.js

--- handlers/event.js
+++ handlers/event.js
@@ -9,5 +9,5 @@ module.exports = (bot) => {
             bot.on(eName, evt.bind(null, bot));
           };
         };
-        ["client", "guild"].forEach(x => load(x));
+        ["client"].forEach(x => load(x));
 };
