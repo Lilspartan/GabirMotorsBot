38256d47f982dcccbd7e7266c1b886c01209d93d
Gabe Krahulik <44793856+Lilspartan@users.noreply.github.com>
2021-08-13 18:03:52 +0000

    added new application command types and image submission

--- events/client/interactionCreate.js
+++ events/client/interactionCreate.js
@@ -1,9 +1,17 @@
 module.exports = async (bot, interaction) => {
-  if (!interaction.isCommand()) return;
-  let commandfile = bot.slashCommands.get(interaction.commandName)
-  try {
-    if (commandfile) commandfile.run(interaction); 
-  } catch (e) {
-    console.log(e)
+  if (interaction.isCommand()) {
+    let commandfile = bot.slashCommands.get(interaction.commandName)
+    try {
+      if (commandfile) commandfile.run(interaction); 
+    } catch (e) {
+      console.log(e)
+    }
+  } else {
+    let commandfile = bot.messageComponents.get(interaction.commandName)
+    try {
+      if (commandfile) commandfile.run(interaction); 
+    } catch (e) {
+      console.log(e)
+    }
   }
 }
